7cff462c623bda37d9b84021d0c1b718ba8c14a5
Cameron Wilby <cameron@origincodeacademy.com>
2017-01-03 12:11:36 -0800

    Added prototype registration feature

--- Frontend/app/register/register.controller.js
+++ Frontend/app/register/register.controller.js
@@ -5,18 +5,34 @@
         .module('app')
         .controller('RegisterController', RegisterController);
 
-    RegisterController.$inject = ['$stateParams'];
+    RegisterController.$inject = ['authFactory', '$state', '$stateParams'];
 
     /* @ngInject */
-    function RegisterController($stateParams) {
+    function RegisterController(authFactory, $state, $stateParams) {
         var vm = this;
         vm.title = 'RegisterController';
 
-        activate();
+        vm.registration = {
+            username: '',
+            emailAddress: '',
+            password: '',
+            confirmPassword: ''
+        };
+
+        vm.register = register;
 
         ////////////////
 
-        function activate() {
+        function register() {
+            authFactory
+                .register(vm.registration)
+                .then(function(response) {
+                    alert('Successful registration! Now login');
+                    $state.go('login');
+                })
+                .catch(function(error) {
+                    alert('Bad registration.');
+                });
         }
     }
 })();
